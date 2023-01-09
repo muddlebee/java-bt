63b52375dfa84d7c631b70989ba04a0ccbed8feb
Anwesh <anweshknayak@gmail.com>
2023-01-09 13:17:22 +0530

    feat(sample):added sample code

--- sample/src/main/java/sample/Main.java
+++ sample/src/main/java/sample/Main.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+package sample;
 import bt.Bt;
 import bt.data.Storage;
 import bt.data.file.FileSystemStorage;
